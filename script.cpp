d994025b3b0e353bd0f1de52d8559a6ac9aa6b93
eurano <106998176+eurano@users.noreply.github.com>
2025-05-05 20:21:07 +0200

    Update script.cpp
    Code rewritten

--- script.cpp
+++ script.cpp
@@ -1,11 +1,30 @@
-#include "script.h"
-#include "natives.h"
-#include "types.h"
-#include "nativeCaller.h"
-#include "enums.h"
+#include "script.h"  
+#include "natives.h"  
+#include "types.h"  
+#include "nativeCaller.h"  
+#include "enums.h"  
+#include <string> 
+
+// Simple range check returns directly
+inline bool isMessShownNow(int toastState) {
+    return toastState >= 0 && toastState <= 5;
+}
 
 void update() {
-    UI::_0x8BC7C1F929D07BF3(0x59C84474);
+    constexpr float DEAD_EYE_THRESHOLD = 0.83f;
+    int toastId = 0;
+    Player player = PLAYER::PLAYER_ID();
+    toastId = UIUNK::_0xC17F69E1418CD11F(1);  // Get toast ID
+
+    if (toastId != 0) {
+        int toastState = UIUNK::_0x59FA676177DBE4C9(toastId);  // Get message type
+        float deadeyeCoreLvl = PLAYER::_0x3A6AE4EEE30370FE(player, false);  // Deadeye level
+
+        if (deadeyeCoreLvl < DEAD_EYE_THRESHOLD && isMessShownNow(toastState)) {
+            UIUNK::_0x2F901291EF177B02(toastId, true);  // Actively block toast
+        }
+    }
+
     WAIT(0);
 }
 
